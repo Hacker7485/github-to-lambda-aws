45fe58074def61237bbd33f7551ab49da9c6ebd6
Ashok Kumar <ashokkumar07116@gmail.com>
2026-05-27 08:33:17 +0530

    pandas latest version1

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,6 @@ import pandas as pd
 def lambda_handler(event,context):
     d = {"col1":[1,2],"col2":[1,2]}
     df = pd.DataFrame(data=d)
-    
+
     print(df)
-    print("Done")
+    print("YES")
